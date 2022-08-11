66f4a6a1340ba8cb0559d9b052deeb7643b576f0
vaniajeronimo <vjeronimo@deloitte.pt>
2022-08-11 15:44:37 +0100

    unit tests setup

--- Podfile
+++ Podfile
@@ -1,13 +1,22 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'Pokemon' do
-  # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
+use_frameworks!
+inhibit_all_warnings!
 
-  # Pods for Pokemon
+def shared_pods
   pod "Alamofire"
   pod 'SnapKit', '~> 4.0.0'
   pod 'SwiftLint'
   pod 'Nimble'
+end
+
+target 'Pokemon' do
+  # Pods for pokemon
+  shared_pods
+end
+
+target 'PokemonTests' do
+    # Pods for testing
+    shared_pods
 end
---
+++ PokemonTests/ViewControllerTests/MainViewControllerTests.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewControllerTests.swift
+//  PokemonTests
+//
+//  Created by Jeronimo, Vania Aguiar on 11/08/2022.
+//
+
+import Foundation
+import XCTest
+import Nimble
+
+@testable import Pokemon
+
+class MainViewControllerTests: XCTestCase {
+    
+    var viewController = MainViewController()
+    
+    override func setUp() {
+        super.setUp()
+        let viewModel = MainViewModel()
+        viewController.viewModel = viewModel
+        viewController.viewDidLoad()
+        viewController.viewWillAppear(true)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        viewController.viewWillDisappear(true)
+        viewController.viewDidDisappear(true)
+    }
+    
+    func testBindViewModel() {
+        
+    }
+}
